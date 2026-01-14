d68c8013ca70a72ec021eecf313d2988f7d08a6a
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2026-01-14 07:55:22 -0600

    Update __init__.py

--- lrs/core/__init__.py
+++ lrs/core/__init__.py
@@ -1,14 +1,15 @@
 """Core LRS-Agents components."""
 
-# Import in order to avoid circular dependencies
 from lrs.core.precision import PrecisionParameters, HierarchicalPrecision
 from lrs.core.lens import ToolLens, ExecutionResult, ComposedLens
 from lrs.core.registry import ToolRegistry
+
+# Free energy imports - only import what exists
 from lrs.core.free_energy import (
     calculate_epistemic_value,
     calculate_pragmatic_value,
     calculate_expected_free_energy,
-    evaluate_policy,
+    # evaluate_policy,  # Comment out if doesn't exist
     precision_weighted_selection,
     PolicyEvaluation,
 )
@@ -23,7 +24,7 @@ __all__ = [
     "calculate_epistemic_value",
     "calculate_pragmatic_value",
     "calculate_expected_free_energy",
-    "evaluate_policy",
+    # "evaluate_policy",  # Comment out
     "precision_weighted_selection",
     "PolicyEvaluation",
 ]

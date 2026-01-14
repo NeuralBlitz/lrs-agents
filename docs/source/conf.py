c9d93261db0fd5352cb1b1aec0b051fc34b6eb20
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2026-01-14 08:54:36 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,16 +2,31 @@
 
 import os
 import sys
+from unittest.mock import MagicMock
 
 # Add project root to path
 sys.path.insert(0, os.path.abspath('../../'))
+
+# Mock optional dependencies for documentation build
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = [
+    'streamlit',
+    'plotly',
+    'plotly.graph_objects',
+    'plotly.express',
+]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
 
 # -- Project information -----------------------------------------------------
 project = 'LRS-Agents'
 copyright = '2024, LRS-Agents Contributors'
 author = 'LRS-Agents Contributors'
-
-# The full version, including alpha/beta/rc tags
 release = '0.2.0'
 version = '0.2.0'
 
@@ -29,22 +44,19 @@ extensions = [
     'nbsphinx',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
 exclude_patterns = [
     '_build',
     'Thumbs.db',
     '.DS_Store',
-    'old',  # Ignore old directory
+    'old',
     '**/old/**',
 ]
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_static_path = []  # Will add _static when needed
+html_static_path = []
 
 html_theme_options = {
     'navigation_depth': 4,
@@ -65,10 +77,6 @@ napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = True
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_type_aliases = None
 
 # Autodoc settings
 autodoc_default_options = {
@@ -77,17 +85,12 @@ autodoc_default_options = {
     'special-members': '__init__',
     'undoc-members': True,
     'exclude-members': '__weakref__',
-    'imported-members': False,  # Don't document imported members
+    'imported-members': False,
 }
 
-# Continue on import errors (for CI)
-autodoc_mock_imports = []
-
-# Suppress warnings about missing imports
-suppress_warnings = [
-    'ref.python',
-    'ref.doc',
-]
+# Don't fail on warnings
+nitpicky = False
+suppress_warnings = ['ref.python', 'ref.doc', 'ref.ref']
 
 # Intersphinx mapping
 intersphinx_mapping = {
@@ -105,5 +108,6 @@ myst_enable_extensions = [
 # nbsphinx settings
 nbsphinx_execute = 'never'
 
-# Allow duplicate labels (temporary fix)
+# Autosummary
 autosummary_generate = True
+autosummary_imported_members = False

b349d5ac09d2529c5dcde3dfc1c1b927c092d58f
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2026-01-15 00:25:58 -0600

    Update __init__.py

--- lrs/integration/__init__.py
+++ lrs/integration/__init__.py
@@ -1,9 +1,7 @@
 """Integration modules for LRS-Agents."""
 
-# Don't import here - causes circular imports
-# Users should import directly from submodules:
-#   from lrs.integration.langgraph import create_lrs_agent
-#   from lrs.integration.langchain_adapter import wrap_langchain_tool
+from lrs.integration.langgraph import create_lrs_agen
+from lrs.integration.langchain_adapter import wrap_langchain_tool
 
 __all__ = [
     "langgraph",

d8c21c9709e9a13046a69b8e06eaf70a2ad2c3ed
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2026-01-15 08:06:10 -0600

    Update __init__.py

--- lrs/integration/__init__.py
+++ lrs/integration/__init__.py
@@ -1,11 +1,8 @@
 """Integration modules for LRS-Agents."""
 
-from lrs.integration.langgraph import create_lrs_agent
-from lrs.integration.langchain_adapter import wrap_langchain_tool
-
 __all__ = [
     "langgraph",
-    "langchain_adapter", 
+    "langchain_adapter",
     "openai_assistants",
     "autogpt_adapter",
 ]
